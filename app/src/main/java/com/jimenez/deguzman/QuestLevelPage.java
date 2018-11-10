5c09a7756a82d3de462cc0aeb82309131e1b5e12
patriciaaadg <patriciadeguzman22@gmail.com>
2018-11-10 13:35:26 +0800

    High Score page

--- app/src/main/java/com/jimenez/deguzman/QuestLevelPage.java
+++ app/src/main/java/com/jimenez/deguzman/QuestLevelPage.java
@@ -1,7 +1,9 @@
 package com.jimenez.deguzman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
 public class QuestLevelPage extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class QuestLevelPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questlevel);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(QuestLevelPage.this, RoundOne.class);
+                QuestLevelPage.this.startActivity(intent);
+                QuestLevelPage.this.finish();
+            }
+        }, 3000);
     }
 }
